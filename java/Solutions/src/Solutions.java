7107b5b4363e8d8d7e71d39182c5e1965bb23711
Alex Hemsath <alex.hemsath@gmail.com>
2017-07-11 13:31:35 -0700

    Learned about {} array initialization, and wrote some more tests.

--- java/Solutions/src/Solutions.java
+++ java/Solutions/src/Solutions.java
@@ -1,8 +1,7 @@
 public class Solutions {
 
 	public int[] twoSum(int[] nums, int target) {
-		int[] returnArray = new int[2];
-		returnArray[0] = -1; returnArray[1] = -1;
+		int[] returnArray = new int[] {-1, -1};
 		for(int i = 0; i<nums.length; i = i + 1) {
 			for(int j = 0; j<nums.length; j = j + 1) {
 				if (i == j) {
--- java/Solutions/src/SolutionsTest.java
+++ java/Solutions/src/SolutionsTest.java
@@ -5,16 +5,29 @@ import org.junit.Test;
 public class SolutionsTest {
 
 	@Test
-	public void simpleTwoSumTest() {
+	public void simpleTest() {
 		Solutions s = new Solutions();
-		int[] expectedIndicies = new int[2];
-		expectedIndicies[0] = 1; expectedIndicies[1] = 2;
-		int target = 6;
-		int[] inputArray = new int[3];
-		inputArray[0] = 3;
-		inputArray[1] = 2;
-		inputArray[2] = 4;
+		int[] expectedIndicies = new int[] {0, 1};
+		int target = 5;
+		int[] inputArray = new int[] {3, 2, 4};
 		assertArrayEquals(expectedIndicies, s.twoSum(inputArray,  target));
 	}
 
+	@Test
+	public void testNoReuse() {
+		Solutions s = new Solutions();
+		int[] expectedIndicies = new int[] {1, 2};
+		int target = 6;
+		int[] inputArray = new int[] {3, 2, 4};
+		assertArrayEquals(expectedIndicies, s.twoSum(inputArray,  target));
+	}
+
+	@Test
+	public void testNotFound() {
+		Solutions s = new Solutions();
+		int[] expectedIndicies = new int[] {-1, -1};
+		int target = 14;
+		int[] inputArray = new int[] {3, 2, 4};
+		assertArrayEquals(expectedIndicies, s.twoSum(inputArray,  target));
+	}
 }
