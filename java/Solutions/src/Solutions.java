590e8e2659c9af4b47aee22f08080503cdc45105
Alex Hemsath <alex.hemsath@gmail.com>
2017-07-12 15:23:58 -0700

    Added some smarter, less O(n^2) solutions to the twoSum() problem.

--- java/Solutions/src/Solutions.java
+++ java/Solutions/src/Solutions.java
@@ -1,7 +1,11 @@
+import java.util.*;
+import java.lang.Math.*;
+
 public class Solutions {
 
 	public int[] twoSum(int[] nums, int target) {
 		int[] returnArray = new int[] {-1, -1};
+		// simple, O(n^2) approach
 		for(int i = 0; i<nums.length; i = i + 1) {
 			for(int j = 0; j<nums.length; j = j + 1) {
 				if (i == j) {
@@ -16,4 +20,48 @@ public class Solutions {
 		}
 		return returnArray;
 	}
-}
+
+	public int[] twoSumSmarter(int[] nums, int target) {
+		int[] returnArray = new int[] {-1, -1};
+		Map<Integer, Integer> map = new HashMap<>();
+		// Pass one: just stuff everything into the map
+		for(int i = 0; i<nums.length; i = i + 1) {
+			map.put(new Integer(nums[i]), new Integer(i));
+		}
+		// Now go back through the array and look for the correct complement in the map
+		for(int i = 0; i<nums.length; i = i + 1) {
+				Integer neededComplement = new Integer(target - nums[i]);
+				if (map.containsKey(neededComplement)) {
+					int indexOfComplement = map.get(neededComplement).intValue();
+					if (indexOfComplement != i) {
+						returnArray[0] = i;
+						returnArray[1] = indexOfComplement;
+						return returnArray;
+					}
+				}
+		}
+		return returnArray;
+	}
+
+	public int[] twoSumSmartest(int[] nums, int target) {
+		int[] returnArray = new int[] {-1, -1};
+		Map<Integer, Integer> map = new HashMap<>();
+		for(int i = 0; i<nums.length; i = i + 1) {
+			map.put(new Integer(nums[i]), new Integer(i));
+			// Oh, wait...why not go ahead and look for the complement right away?
+			Integer neededComplement = new Integer(target - nums[i]);
+			if (map.containsKey(neededComplement)) {
+				int indexOfComplement = map.get(neededComplement).intValue();
+				if (indexOfComplement != i) {
+					// One difference here is that we need to make the order correct (lower index first, then higher)
+					// Since the indexes are ordered and can't be the same, just take the min and max
+					returnArray[0] = Math.min(i, indexOfComplement);
+					returnArray[1] = Math.max(i, indexOfComplement);
+					return returnArray;					
+				}
+			}
+		}
+		return returnArray;
+	}
+
+}	
--- java/Solutions/src/SolutionsTest.java
+++ java/Solutions/src/SolutionsTest.java
@@ -30,4 +30,59 @@ public class SolutionsTest {
 		int[] inputArray = new int[] {3, 2, 4};
 		assertArrayEquals(expectedIndicies, s.twoSum(inputArray,  target));
 	}
+	
+	@Test
+	public void simpleTestSmarter() {
+		Solutions s = new Solutions();
+		int[] expectedIndicies = new int[] {0, 1};
+		int target = 5;
+		int[] inputArray = new int[] {3, 2, 4};
+		assertArrayEquals(expectedIndicies, s.twoSumSmarter(inputArray,  target));
+	}
+
+	@Test
+	public void testNoReuseSmarter() {
+		Solutions s = new Solutions();
+		int[] expectedIndicies = new int[] {1, 2};
+		int target = 6;
+		int[] inputArray = new int[] {3, 2, 4};
+		assertArrayEquals(expectedIndicies, s.twoSumSmarter(inputArray,  target));
+	}
+
+	@Test
+	public void testNotFoundSmarter() {
+		Solutions s = new Solutions();
+		int[] expectedIndicies = new int[] {-1, -1};
+		int target = 14;
+		int[] inputArray = new int[] {3, 2, 4};
+		assertArrayEquals(expectedIndicies, s.twoSumSmarter(inputArray,  target));
+	}
+
+	@Test
+	public void simpleTestSmartest() {
+		Solutions s = new Solutions();
+		int[] expectedIndicies = new int[] {0, 1};
+		int target = 5;
+		int[] inputArray = new int[] {3, 2, 4};
+		assertArrayEquals(expectedIndicies, s.twoSumSmartest(inputArray,  target));
+	}
+
+	@Test
+	public void testNoReuseSmartest() {
+		Solutions s = new Solutions();
+		int[] expectedIndicies = new int[] {1, 2};
+		int target = 6;
+		int[] inputArray = new int[] {3, 2, 4};
+		assertArrayEquals(expectedIndicies, s.twoSumSmartest(inputArray,  target));
+	}
+
+	@Test
+	public void testNotFoundSmartest() {
+		Solutions s = new Solutions();
+		int[] expectedIndicies = new int[] {-1, -1};
+		int target = 14;
+		int[] inputArray = new int[] {3, 2, 4};
+		assertArrayEquals(expectedIndicies, s.twoSumSmartest(inputArray,  target));
+	}
+
 }
